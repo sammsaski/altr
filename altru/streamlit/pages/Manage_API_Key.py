a51d022ce492a32d7818e48a66d4f3b1161834ad
sammsaski <samuel.sasaki@gmail.com>
2023-08-21 18:59:18 -0500

    things working well, but big change coming following this commit

--- altru/streamlit/pages/Manage_API_Key.py
+++ altru/streamlit/pages/Manage_API_Key.py
@@ -24,21 +24,17 @@ db = firestore.Client.from_service_account_json(os.getcwd() + '/firestore-key.js
 # TODO: use session state NOT config file for loading api_key
 
 username = st.session_state['username']
-# api_key = config['credentials']['usernames'][username]['api_key']
-
 doc = db.collection('users').document(username)
 api_key = doc.get().to_dict()['api_key']
 
 st.subheader('Submit/Edit Scraper API Key')
 
-# if not config['credentials']['usernames'][username]['api_key']:
-#     print('YAY!!!')    
-
-# print(config['credentials']['usernames'][username])
-
 view_key, add_key, edit_key, delete_key = st.tabs(['View', 'Add', 'Edit', 'Remove'])
 
 
+#--------------------
+#--- VIEW API_KEY ---
+#--------------------
 with view_key:
     api_keys =  db.collection('api_keys').where(filter=FieldFilter('user', '==', username)).stream()
     api_keys = {x.to_dict()['name']: x.to_dict()['api_key'] for x in api_keys} # HACK: maybe not the most efficient
@@ -50,6 +46,10 @@ with view_key:
 
     st.code(api_keys[option], language='markdown')
 
+
+#-------------------
+#--- ADD API_KEY ---
+#-------------------
 with add_key:
     st.write('After adding your API key, please navigate to the `View` tab to ensure that the API key was added successfully.')
 
@@ -68,40 +68,49 @@ with add_key:
                 st.success('Hooray! API key addition succeeded!')
 
 # TODO: FIX THIS
+#--------------------
+#--- EDIT API_KEY ---
+#--------------------
 with edit_key:
-    api_keys =  db.collection('api_keys').where(filter=FieldFilter('user', '==', username)).stream()
-    name_to_id = {x.to_dict()['name']: x.id for x in api_keys}
+    api_keys = db.collection('api_keys').where(filter=FieldFilter('user', '==', username)).stream()
     api_keys = {x.to_dict()['name']: (x.to_dict()['api_key'], x.id) for x in api_keys} # HACK: maybe not the most efficient
+
     option = st.selectbox(
         'Select an API Key to edit.',
-        [name for name in api_keys.keys()],
+        api_keys.keys(),
         key='edit_key'
     )
 
     with st.form(key='edit_key_form', clear_on_submit=True):
         updated_api_key_name = st.text_input('API Key Name', value=option)
-        updated_api_key = st.text_input('API Key', value=api_keys[option])
+        updated_api_key = st.text_input('API Key', value=api_keys[option][0])
         submit_button = st.form_submit_button('Edit')
     
         if submit_button:
-            db.collection('api_keys').document(name_to_id[option]).update({'name': updated_api_key_name})
-            db.collection('api_keys').document(name_to_id[option]).update({'api_key': updated_api_key})
+            db.collection('api_keys').document(api_keys[option][1]).update({'name': updated_api_key_name})
+            db.collection('api_keys').document(api_keys[option][1]).update({'api_key': updated_api_key})
             st.experimental_rerun()
 
+#----------------------
+#--- DELETE API_KEY ---
+#----------------------
 with delete_key:
-    pass
+    api_keys = db.collection('api_keys').where(filter=FieldFilter('user', '==', username)).stream()
+    api_keys = {x.to_dict()['name']: (x.to_dict()['api_key'], x.id) for x in api_keys} # HACK: maybe not the most efficient
 
-# if not api_key:
+    option = st.selectbox(
+        'Select an API Key to edit.',
+        api_keys.keys(),
+        key='delete_key'
+    )
 
-#     # saves to api_key on 'enter'
-#     # placeholder = st.empty()
-#     # with placeholder.container():
-#     api_key = st.text_input("Please input your Scraper API key.", key="api_key_form")
-#     api_key_edit_button = st.button('Add', key='add_api_key_button')
-#     if api_key_edit_button:
-#         doc.update({'api_key': api_key})
-#         st.experimental_rerun()
-    
-# else:
-#     st.code(api_key, language='markdown')
+    st.code(api_keys[option][0], language='markdown')
 
+    delete_button = st.button('Delete')
+
+    if delete_button:
+        # delete the selected api_key
+        db.collection('api_keys').document(api_keys[option][1]).delete()
+
+        # reload the page
+        st.experimental_rerun()
