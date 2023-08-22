ec28dfe6b2120334fb61b76141a8e8415c5de692
Samuel Sasaki <sasakis@Samuels-Mac-Studio.local>
2023-08-22 18:24:03 -0500

    Getting closer to working

--- altru/comparison/compare.py
+++ altru/comparison/compare.py
@@ -1,156 +1,24 @@
-from difflib import Differ
-import re
+from fuzzywuzzy import fuzz
 
 
 class PropertyComparison:
     """
     This class contains the methods necessary to compare different attributes of a property.
 
-    Some irregular behavior that needs to be implemented include:
-    - comparison of comma separated strings (address)
-    - comparison of a string with a $ in it (price)
-    - comparison of floats within strings (acreage)
-
     For these comparisons, the goal is to take in two inputs and output a boolean denoting the whether or not
     the input strings are equivalent.
     """    
 
-    # NOTE: For now, this works under the format : <street>, <county>, <state> <zip>
-    def compare_comma_separated_string(s: str, t: str) -> bool:
-        """Compare the components of a comma separated string.
+    # TODO: Add type annotations
+    @staticmethod
+    def equal(s, t):
 
-        s (str) : the scraped data.
-        t (str) : the true data.
-
-        returns (bool) : whether or not the scraped and true data are equivalent.
-        """
-        return s.split(", ") == t.split(", ")
+        # Covers price, bedrooms, bathrooms, sqft, acre, year_built
+        if (type(s) == type(t) == int) or (type(s) == type(t) == float):
+            return s == t
+        
+        # Covers address
+        # TODO: Need a better comparison tool for this. fuzzy string matching will work for the street address, but everything else will need to be exact.
+        if type(s) == type(t) == str:
+            return fuzz.token_sort_ratio(s, t) > 90
     
-    def compare_dollar_amount(s: str, t: str) -> bool:
-        """Check the equality of two dollar amounts.
-        
-        s (str) : the scraped data
-        t (str) : the true data
-
-        returns (bool) : whether or not the scraped and true data are equivalent.
-        """
-        temp_t = re.sub('[^0-9]','', t)
-        temp_s = re.sub('[^0-9]','', s)
-
-        eq = int(temp_t) == int(temp_s)
-
-        return (s, f':green[{t}]') if eq else (f':red[{s}]', t)
-
-    # NOTE: For now, this works under the format : <float value> <unit>
-    def compare_float_string(s: str, t: str) -> bool:
-        """Compare the components of a string containing a floating point value.
-        
-        s (str) : the scraped data.
-        t (str) : the true data.
-
-        returns (bool) : whether or not the scraped and true data are equivalent.
-        """
-        s = s.split(" ")
-        t = t.split(" ")
-    
-        # NOTE: Add support for conversion.
-
-        # unpack by value and unit
-        sv, su = s
-        tv, tu = t
-
-        # Handles the case where one is 'acres' and the other is 'acre'.
-        if (su in tu) or (tu in su):
-            return float(sv) == float(tv)
-        else:
-            # NOTE: Need to do.
-            pass 
-
-
-    def compare_address(t, s):
-        if t == s:
-            return True, []
-        
-        # len of t and s should be the same after split.
-        t = t.split(",")
-        s = s.split(",")
-
-        d = Differ()
-
-        differences = [list(d.compare(x, y)) for x, y in zip(t, s)]
-
-        wrong = ''
-        right = ''
-
-        for i, diff in enumerate(differences):
-
-            diff = list(map(str.strip, diff))
-            diff = list(map(lambda x: x.replace('', ' ') if x == '' else x, diff))
-
-            if t[i] == ''.join(diff):
-                wrong += f'{t[i]}, '
-                right += f'{t[i]}, '
-            else:
-                wrong += f':red[{s[i]}], '
-                right += f':green[{t[i]}], '
-        
-        return wrong[:-2], right[:-2]
-
-    # NOTE: rename 'wrong' and 'right' variables. it is misleading.
-    def compare_price(t, s):
-        """
-        """
-        temp_t = re.sub('[^0-9]','', t)
-        temp_s = re.sub('[^0-9]','', s)
-
-        eq = int(temp_t) == int(temp_s)
-
-        return (s, f':green[{t}]') if eq else (f':red[{s}]', t)
-
-        
-
-
-    # NOTE: Add type annotations
-    # NOTE: Maybe add a better name. This doesn't exactly highlight what is happening.
-    def compare_bedrooms(t: str, s: str):
-        """Return colored strings based on the evaluation of the comparison between
-        both the true and scraped input data.
-
-        This method expects that the inputs can be directly type cast to 'int'.
-        """
-        return (s, f':green[{t}]') if int(s) == int(t) else (f':red[{s}]', t)
-        
-
-    # NOTE: Add type annotations
-    # NOTE: Maybe add a better name. This doesn't exactly highlight what is happening.
-    def compare_bathrooms(t: str, s: str):
-        """Return colored strings based on the evaluation of the comparison between
-        both the true and scraped input data.
-
-        This method expects that the inputs can be directly type cast to 'int'.
-        """
-        return (s, f':green[{t}]') if int(s) == int(t) else (f':red[{s}]', t)
-        
-    def compare_sqft(t: str, s: str):
-        t = re.sub('[^0-9]','', t)
-        s = re.sub('[^0-9]','', s)
-
-        # check equality
-        eq = int(s) == int(t)
-
-        s = '{:,}'.format(int(s))
-        t = '{:,}'.format(int(t))
-
-        return (s, f':green[{t}]') if eq else (f':red[{s}]', t)
-
-    def compare_acre(t: str, s: str):
-        temp_t = t.split(" ")
-        temp_s = s.split(" ")
-
-        # check equality
-        eq = float(temp_t[0]) == float(temp_s[0])
-
-        return (s, f':green[{t}]') if eq else (f':red[{s}]', t)
-        
-    def compare_year_built(t: str, s: str):
-        return (s, f':green[{t}]') if int(s) == int(t) else (f':red[{s}]', t)
--- altru/streamlit/pages/Run_Audit.py
+++ altru/streamlit/pages/Run_Audit.py
@@ -2,9 +2,15 @@ import os
 import streamlit as st
 from google.cloud import firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
+
 from altru.zillow.zillow_scraper import ZillowScraper
+from altru.audit.audit import Audit
 
 st.title('Run Audit')
+
+# TODO: Implement this structure.
+def audit_report_single_property_structure(address, price, bedrooms, bathrooms, sqft, acre, year_built):
+    pass
 
 # authenticate user first
 if st.session_state['authentication_status']:
@@ -29,11 +35,56 @@ if st.session_state['authentication_status']:
         key='listing_sites_to_audit'
     )
 
-    run_audit = st.button('Run!')
+    api_keys =  db.collection('api_keys').where(filter=FieldFilter('user', '==', username)).stream()
+    api_keys = {x.to_dict()['name']: x.to_dict()['api_key'] for x in api_keys} # HACK: maybe not the most efficient
+    api_key = st.selectbox(
+        'Select an API Key to use.',
+        [name for name in api_keys.keys()],
+        key='audit_api_key'
+    )
+
+    api_key = db.collection('api_keys').where(filter=FieldFilter('user', '==', username)).where(
+        filter=FieldFilter('name', '==', api_key)
+    ).get()[0].get(field_path='api_key') # TODO: we need database guarantees that this will return only one object.
+
+    properties = db.collection('audits').where(filter=FieldFilter('user', '==', username)).where(
+        filter=FieldFilter('name', '==', option)
+    ).get()[0].get(field_path='properties') # TODO: we need database guarantees that this will return only one object.
+
+    # for property in properties:
+    #     print(property.get())
+
+    # NOTE :- use .get(field_path) on a DocumentSnapshot to get specific information.
+
+    run_audit = st.button('Run!') # TODO: Disable button after run, UNLESS there is some input change.
 
     if run_audit:
         # TODO: Implement the run audit functionality.
-        pass
+        audit = Audit(
+            properties=properties,
+            listing_sites=listing_sites_to_audit,
+            api_key=api_key
+        )
+
+        result, correct, total = audit.execute()
+        
+        # TODO: Copy over from streamlit/pages/comparison.py how to format the results of the audit.
+        # for property in result:
+        #     st.write(f'{property} : {result[property]}')
+
+        for result_dict in result.values():
+            for key, value in result_dict.items():
+                # if key == 'address':
+                #     st.subheader(value)
+                # elif key == ''
+                st.write(f'{key} : {value}')
+            st.divider()
+        
+        st.subheader('Analytics')
+        col = st.columns(3)
+        col[0].metric('accuracy', '{:.2f}'.format(correct / total * 100) + '%')
+        col[1].metric('correct', correct)
+        col[2].metric('total', total)
 
 else:
     st.error('You must be logged in to access this page.')
